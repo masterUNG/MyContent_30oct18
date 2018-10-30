de17ac8d90866a1339ebd9d0903d12fe3c98067d
masterUNG <phrombutr@gmail.com>
2018-10-30 15:49:21 +0700

    finish 30oct

--- MyContent/ViewController.swift
+++ MyContent/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //    Explicit
+    var userString: String?
+    var passwordString: String?
+    
+    
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+        userString = userTextField.text
+        passwordString = passwordTextField.text
+        showLog()
+        
+    }   // signIn
+    
+    func showLog() -> Void {
+        print("user ==> \(userString!)")
+        print("password ==> \(passwordString!)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
 
