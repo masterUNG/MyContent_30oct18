f3353e2d41f3938c68d70044f2040b4ed11276f3
masterUNG <phrombutr@gmail.com>
2018-10-30 13:16:51 +0700

    Check Space OK

--- MyContent/RegisterViewController.swift
+++ MyContent/RegisterViewController.swift
@@ -9,22 +9,67 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //    Explicit
+    var nameString: String = ""
+    var userString: String = ""
+    var passwordString: String = ""
+    
+    
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func uploadButton(_ sender: Any) {
+        
+        nameString = nameTextField.text!
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
+        showLogString()
+        
+        if checkString() {
+//            Have Space
+            
+        } else {
+//            No Space
+            
+        }
+        
+        
+        
+    }   // upload
+    
+    func checkString() -> Bool {
+        
+        var result: Bool?
+        
+        if (nameString.count == 0) || (userString.count == 0) || (passwordString.count == 0) {
+            result = true
+        } else {
+            result = false
+        }
+        
+        print("checkString ==> \(result!)")
+        return result!
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showLogString() -> Void {
+        print("name ==> \(nameString)")
+        print("user ==> \(userString)")
+        print("password ==> \(passwordString)")
     }
-    */
-
-}
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }   // viewDidLoad
+    
+}   // Main Class
